75ead678136269b7c5bb606fb311e02c2395ce4e
haappi <74637670+haappi@users.noreply.github.com>
2022-08-22 18:09:24 -0500

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.github.haappi.fivechallenges {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens io.github.haappi.fivechallenges to javafx.fxml;
+    exports io.github.haappi.fivechallenges;
+}
