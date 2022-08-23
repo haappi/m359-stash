e43957b9b112fb311225c1d0203e1da759811eb9
haappi <74637670+haappi@users.noreply.github.com>
2022-08-22 21:16:24 -0500

    Main controller

--- src/main/java/io/github/haappi/fivechallenges/HelloApplication.java
+++ src/main/java/io/github/haappi/fivechallenges/HelloApplication.java
@@ -1,6 +1,9 @@
 package io.github.haappi.fivechallenges;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,11 +12,42 @@ import javafx.stage.Stage;
 public class HelloApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello!");
-    stage.setScene(scene);
-    stage.show();
+    System.out.println("""
+            Select an option:
+            1. Breeding of bugs.
+            2. Area of a circle.
+            3. Salary simulation.
+            4. Shop simulator.
+            5. Calculate roots of quadratic equation.
+            6. Exit.
+            """);
+    BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+
+    String name = reader.readLine();
+    switch (name) {
+        case "1":
+            Bugs.run(stage);
+            break;
+        case "2":
+            AreaOfACircle.run(stage);
+            break;
+        case "3":
+            SalarySimulation.run(stage);
+            break;
+        case "4":
+            ShopSimulation.run(stage);
+            break;
+        case "5":
+            CalculateRootsOfQuadraticEquation.run(stage);
+            break;
+        case "6":
+            System.exit(0);
+            break;
+        default:
+            System.out.println("Invalid option");
+            break;
+    }
   }
 
   public static void main(String[] args) {
