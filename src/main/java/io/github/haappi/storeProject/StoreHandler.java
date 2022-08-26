73adacf61a2411a31c446037e10a4400a114abb2
haappi <74637670+haappi@users.noreply.github.com>
2022-08-26 12:43:10 -0500

    base things

--- src/main/java/io/github/haappi/storeProject/StoreHandler.java
+++ src/main/java/io/github/haappi/storeProject/StoreHandler.java
@@ -1,12 +1,17 @@
 package io.github.haappi.storeProject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class StoreHandler {
+  @FXML
+  protected Text output;
+
   public StoreHandler() {
     System.out.println("hi");
   }
@@ -14,13 +19,25 @@ public class StoreHandler {
   @FXML
   public void initialize() {
     System.out.println("hii");
+    output.setText("ooh ooh ah ah"); // this successfully changes the value of the Text without it complaining
   }
-  @FXML private Label welcomeText;
 
   @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+  protected void addProductOne() {
   }
+  @FXML
+  protected void addProductTwo() {
+  }
+  @FXML
+  protected void addProductThree() {
+  }
+  @FXML
+  protected void addProductFour() {
+  }
+  @FXML
+  protected void purchaseItems() {
+  }
+
   /*
   Random number everytime program inits
   When returning change, show the user the actual cash (54 -> 2 twenties, 1 ten, 4 dollars)
