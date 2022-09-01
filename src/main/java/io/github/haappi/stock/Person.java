e366101c0cc1b1a8b7c8a670a9b4363b42f50394
haappi <74637670+haappi@users.noreply.github.com>
2022-08-31 21:49:55 -0500

    fix

--- src/main/java/io/github/haappi/stock/Person.java
+++ src/main/java/io/github/haappi/stock/Person.java
@@ -3,10 +3,15 @@ package io.github.haappi.stock;
 public class Person {
   private final String name;
   private Stock stockOne;
-  private Integer stockOneAmount;
+  private Integer stockOneAmount = 0;
 
   private Stock stockTwo;
-  private Integer stockTwoAmount;
+  private Integer stockTwoAmount = 0;
+
+  public Double getWallet() {
+    return wallet;
+  }
+
   private Double wallet = 0.00;
 
   public Person(String name) {
@@ -26,12 +31,51 @@ public class Person {
     return stockTwoAmount;
   }
 
-  public Integer changeStockOneAmount(Integer amount) {
-    return stockOneAmount += amount;
+
+  public String changeStockOne(Integer amount) {
+    if (amount < 0 && stockOneAmount < 0) {
+      return "You don't have any shares of " + stockOne.getName() + "!";
+    }
+
+    stockOneAmount += amount;
+    String message;
+    if (amount < 0) {
+      wallet += stockOne.getPrice();
+      message = "You sold 1 share of " + stockOne.getName() + " for $" + stockOne.getPrice();
+    } else {
+      if (wallet < stockOne.getPrice()) {
+        stockOneAmount -= amount;
+        message = "You don't have enough money to buy a share of " + stockOne.getName() + "!";
+      } else {
+        wallet -= stockOne.getPrice();
+        message = "You bought 1 share of " + stockOne.getName() + " for $" + stockOne.getPrice();
+      }
+    }
+
+    return message;
   }
 
-  public Integer changeStockTwoAmount(Integer amount) {
-    return stockTwoAmount += amount;
+public String decreaseStockTwo(Integer amount) {
+  if (amount < 0 && stockTwoAmount < 0) {
+    return "You don't have any shares of " + stockTwo.getName() + "!";
+  }
+
+  stockTwoAmount += amount;
+  String message;
+  if (amount < 0) {
+    wallet += stockTwo.getPrice();
+    message = "You sold 1 share of " + stockTwo.getName() + " for $" + stockTwo.getPrice();
+  } else {
+    if (wallet < stockTwo.getPrice()) {
+      stockTwoAmount -= amount;
+      message = "You don't have enough money to buy a share of " + stockTwo.getName() + "!";
+    } else {
+      wallet -= stockTwo.getPrice();
+      message = "You bought 1 share of " + stockTwo.getName() + " for $" + stockTwo.getPrice();
+    }
+  }
+
+  return message;
   }
 
   public String getName() {
@@ -55,7 +99,14 @@ public class Person {
   }
 
   public Double getNetworth() {
-    return stockOne.getPrice() + stockTwo.getPrice() + wallet;
+    Double worth = wallet;
+    if (stockOne != null) {
+      worth += stockOne.getPrice() * stockOneAmount;
+    }
+    if (stockTwo != null) {
+      worth += stockTwo.getPrice() * stockTwoAmount;
+    }
+    return worth;
   }
 
   public void setWallet(Double wallet) {
