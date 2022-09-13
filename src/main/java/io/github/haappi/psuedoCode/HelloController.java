76efac4e5da2bfd4277b5e9781e8ea99b6cfbc7c
haappi <74637670+haappi@users.noreply.github.com>
2022-09-13 12:43:26 -0500

    bruuuh

--- src/main/java/io/github/haappi/psuedoCode/HelloController.java
+++ src/main/java/io/github/haappi/psuedoCode/HelloController.java
@@ -9,5 +9,25 @@ public class HelloController {
   @FXML
   protected void onHelloButtonClick() {
     welcomeText.setText("Welcome to JavaFX Application!");
+    System.out.println(generateFibonacci(6));
+  }
+
+  private String generateFibonacci(int count) {
+    int a = 1;
+    int b = 1;
+
+    String output = "";
+    for (int i = 0; i < count; i++) {
+      output = doSomething(output, a, b, a + b);
+      int temp = b;
+      b = a + b;
+      a = temp;
+    }
+    return output;
+  }
+
+  private String doSomething(String string, int a, int b, int c) {
+    string = string + a + ", " + b + ", " + c + ", ";
+    return string;
   }
 }
