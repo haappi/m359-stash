5c6e3e2f35ea3b894209965718ae021639eef073
haappi <74637670+haappi@users.noreply.github.com>
2022-10-10 12:31:37 -0500

    Ooops

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,9 @@ module io.github.haappi.battleGame {
   requires javafx.controls;
   requires javafx.fxml;
   requires java.desktop;
+  requires com.google.gson;
 
-  opens io.github.haappi.battleGame to
+    opens io.github.haappi.battleGame to
       javafx.fxml;
 
   exports io.github.haappi.battleGame;
