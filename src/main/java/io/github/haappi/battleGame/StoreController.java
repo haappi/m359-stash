bbe8a56b25c37251419ca3776cad1720e8f1aae4
haappi <74637670+haappi@users.noreply.github.com>
2022-10-13 12:43:59 -0500

    more useless additions!!!!

---
+++ src/main/java/io/github/haappi/battleGame/StoreController.java
@@ -0,0 +1,11 @@
+package io.github.haappi.battleGame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+
+public class StoreController {
+    @FXML
+    protected ListView storeItems;
+    @FXML protected Text currentlyShopping;
+}
