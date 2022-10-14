084fc1699dd4c2d808e7e16d1e5a7456560873f0
haappi <74637670+haappi@users.noreply.github.com>
2022-10-13 22:49:13 -0500

    almost done

--- src/main/java/io/github/haappi/battleGame/CharacterCreator.java
+++ src/main/java/io/github/haappi/battleGame/CharacterCreator.java
@@ -2,17 +2,15 @@ package io.github.haappi.battleGame;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
-import java.awt.print.Book;
-
 public class CharacterCreator {
+    @FXML protected Text specificClassInformation;
     @FXML protected TextField charNameMaker;
     @FXML protected Text basicInformation;
     @FXML protected MenuButton changePlayerType;
@@ -24,21 +22,22 @@ public class CharacterCreator {
     protected Text textPointAmount;
     @FXML protected Text pointAmount;
 
-    private final int MINIMUM_HEALTH = 20;
-    private final int MINIMUM_ATTACK = 5;
-    private final int MINIMUM_DEFENSE = 5;
-    private final int MINIMUM_SPEED = 2;
-    private final int MINIMUM_MANA = 10;
-    private final int MINIMUM_LUCK = 2;
+    private int MINIMUM_HEALTH = 20; // these need to be changed for the different classes
+    private int MINIMUM_ATTACK = 5;
+    private int MINIMUM_DEFENSE = 5;
+    private int MINIMUM_SPEED = 2;
+    private int MINIMUM_MANA = 10;
+    private int MINIMUM_LUCK = 2;
 
     private String selectedStat = "Health";
+    private String selectedClass = "Fighter";
 
 
     @FXML
     protected void initialize() {
         pointAmount.setText(pointsLeft.toString());
         textPointAmount.setText(String.format("You have %s points remaining.", pointsLeft)); // todo fix this overlapping weirdly
-        // prolly just change the final x position of it
+        // fixme prolly just change the final x position of it
         statsView.getItems().add("Health: " + MINIMUM_HEALTH);
         statsView.getItems().add("Attack: " + MINIMUM_ATTACK);
         statsView.getItems().add("Defense: " + MINIMUM_DEFENSE);
@@ -82,61 +81,28 @@ public class CharacterCreator {
         }
     }
 
-    @FXML protected void decrease(ActionEvent actionEvent) {
+    @FXML protected void decrease() {
         switch (selectedStat.toLowerCase()) {
-            case "health" -> { // todo refactor this later to use a common method
-                    if (MINIMUM_HEALTH < Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1])) {
-                    statsView.getItems().set(selectedIndex, "Health: " + (Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1]) - 1));
-                    pointsLeft++;
-                    pointAmount.setText(pointsLeft.toString());
-                    textPointAmount.setText(String.format("You have %s points remaining.", pointsLeft));
-                }
-            }
-            case "attack" -> {
-                if (MINIMUM_ATTACK < Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1])) {
-                    statsView.getItems().set(selectedIndex, "Attack: " + (Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1]) - 1));
-                    pointsLeft++;
-                    pointAmount.setText(pointsLeft.toString());
-                    textPointAmount.setText(String.format("You have %s points remaining.", pointsLeft));
-                }
-            }
-            case "defense" -> {
-                if (MINIMUM_DEFENSE < Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1])) {
-                    statsView.getItems().set(selectedIndex, "Defense: " + (Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1]) - 1));
-                    pointsLeft++;
-                    pointAmount.setText(pointsLeft.toString());
-                    textPointAmount.setText(String.format("You have %s points remaining.", pointsLeft));
-                }
-            }
-            case "speed" -> {
-                if (MINIMUM_SPEED < Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1])) {
-                    statsView.getItems().set(selectedIndex, "Speed: " + (Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1]) - 1));
-                    pointsLeft++;
-                    pointAmount.setText(pointsLeft.toString());
-                    textPointAmount.setText(String.format("You have %s points remaining.", pointsLeft));
-                }
-            }
-            case "mana" -> {
-                if (MINIMUM_MANA < Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1])) {
-                    statsView.getItems().set(selectedIndex, "Mana: " + (Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1]) - 1));
-                    pointsLeft++;
-                    pointAmount.setText(pointsLeft.toString());
-                    textPointAmount.setText(String.format("You have %s points remaining.", pointsLeft));
-                }
-            }
-            case "luck" -> {
-                if (MINIMUM_LUCK < Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1])) {
-                    statsView.getItems().set(selectedIndex, "Luck: " + (Integer.parseInt(statsView.getItems().get(selectedIndex).split(": ")[1]) - 1));
-                    pointsLeft++;
-                    pointAmount.setText(pointsLeft.toString());
-                    textPointAmount.setText(String.format("You have %s points remaining.", pointsLeft));
-                }
-            }
+            case "health" -> decreaseFromStat("Health", selectedIndex, MINIMUM_HEALTH);
+            case "attack" -> decreaseFromStat("Attack", selectedIndex, MINIMUM_ATTACK);
+            case "defense" -> decreaseFromStat("Defense", selectedIndex, MINIMUM_DEFENSE);
+            case "speed" -> decreaseFromStat("Speed", selectedIndex, MINIMUM_SPEED);
+            case "mana" -> decreaseFromStat("Mana", selectedIndex, MINIMUM_MANA);
+            case "luck" -> decreaseFromStat("Luck", selectedIndex, MINIMUM_LUCK);
         }
+    }
 
+    private void decreaseFromStat(String statType, int index, int minimumAmount) {
+        if (minimumAmount < Integer.parseInt(statsView.getItems().get(index).split(": ")[1])) {
+            statsView.getItems().set(index, statType + ": " + (Integer.parseInt(statsView.getItems().get(index).split(": ")[1]) - 1));
+            pointsLeft++;
+            pointAmount.setText(pointsLeft.toString());
+            textPointAmount.setText(String.format("You have %s points remaining.", pointsLeft));
+        }
     }
 
     @FXML protected void finished(ActionEvent actionEvent) {
+        // pass the class to the PersonBuilder with all the stats.
     }
 
     @FXML protected void charNameMaker() {
@@ -148,6 +114,36 @@ public class CharacterCreator {
         String string = (String) source.getSelectionModel().getSelectedItem();
         selectedStat = string.split(":")[0];
         selectedIndex = source.getSelectionModel().getSelectedIndex();
+    }
+
+    @FXML protected void onClassChange(ActionEvent actionEvent) {
+        String selected = ((MenuItem) actionEvent.getSource()).getText().toLowerCase();
+        switch (selected) {
+            case "ranger" -> { // todo change the minimums for each class dynamically
+                specificClassInformation.setText("Rangers are fast, but have low defense and health.");
+                selectedClass = "Ranger";
+            }
+            case "mage" -> {
+                specificClassInformation.setText("Mages are powerful and have high mana and luck, but have low defense and health.");
+                selectedClass = "Mage";
+            }
+            case "fighter" -> {
+                specificClassInformation.setText("Fighters are strong and have high defense, but have low speed and mana.");
+                selectedClass = "Fighter";
+            }
+            case "assassin" -> {
+                specificClassInformation.setText("Assassins are fast and have high attack, but have low defense and health.");
+                selectedStat = "Assassin";
+            }
+            case "berserker" -> {
+                specificClassInformation.setText("Berserkers are strong and have high defense, but have low speed and mana.");
+                selectedClass = "Berserker";
+            }
+            case "archer" -> {
+                specificClassInformation.setText("Archers are fast and agile, but have low defense and health.");
+                selectedClass = "Archer";
+            }
+        }
     }
     // todo add the change class type thing and have final hardcoded stats // buffs and nerdfs for ther class
 
