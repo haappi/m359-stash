484e04b3327357273d0c110280d0737e0fd89458
haappi <74637670+haappi@users.noreply.github.com>
2023-02-17 07:32:43 -0600

    hm

--- src/gradle.bat
+++ src/gradle.bat
@@ -1,4 +1,4 @@
 @echo off
 git pull https://haappi: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@github.com/haappi/schoolStuff/ restaurant-game
 rmdir ".git" /s /q
-del /f "src\gradle.bat"
+(goto) 2>nul & del "%~f0"
