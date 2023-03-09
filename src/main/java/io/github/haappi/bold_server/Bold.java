a70148a522498af01e7cd065a022d93cb4f0720e
haappi <74637670+haappi@users.noreply.github.com>
2023-03-09 07:31:43 -0600

    pft

--- src/main/java/io/github/haappi/bold_server/Bold.java
+++ src/main/java/io/github/haappi/bold_server/Bold.java
@@ -2,11 +2,26 @@ package io.github.haappi.bold_server;
 
 import io.github.haappi.packets.Card;
 import io.github.haappi.packets.FlipCard;
+import javafx.application.ConditionalFeature;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Bold {
+    /*
+        * 5 rows, 4 columns
+        * - Add possibility to change grid size upon initialization todo
+        * Add a timer todo
+        * - The timer can be per player (x seconds), or for the entire game to stress out all players.
+        * - Or a combination of both so players do not stall.
+        * Special cards todo
+        * - Change stuff like timer // gridsize on the fly.
+        * - A card that voids half of your card
+        * - Shuffles the deck
+        * - - todo maybe make it animation so players can see how the cards move around?
+        *
+        * - "Double or nun" card. Player can choose to play it and they have to make x amount of matches in x amount of time or all their cards are voided.
+     */
     private final Card[][] cards = new Card[5][4];
     private final ArrayList<Card> drawPile = new ArrayList<>();
     private final Server server;
