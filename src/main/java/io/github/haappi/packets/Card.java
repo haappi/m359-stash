3c58fe9467de2a2f290b98fc618a8866f6033070
haappi <74637670+haappi@users.noreply.github.com>
2023-03-16 07:21:59 -0500

    bagh

--- src/main/java/io/github/haappi/packets/Card.java
+++ src/main/java/io/github/haappi/packets/Card.java
@@ -4,11 +4,15 @@ import static io.github.haappi.shared.Utils.imageHashMap;
 
 import io.github.haappi.shared.Enums;
 
+import javafx.animation.RotateTransition;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
 
 import java.io.Serial;
 
-public class Card implements Packet {
+public class Card extends ImageView implements Packet {
     @Serial private static final long serialVersionUID = 5839422576067187289L;
     private final String fileURI;
     private final String cardName;
@@ -17,24 +21,55 @@ public class Card implements Packet {
     private final Enums color;
     private final Enums container;
     private final Enums pattern;
-    private final boolean isBackCard;
 
-    private boolean isFlipped = false;
+    private int x;
+    private int y;
+
+    /**
+     * If this is a False it means the card is showing its face.<br>
+     * Else if it's True, it means the card is showing its back.
+     */
+    private boolean isFlipped = true;
+    public final static String backCardURI = "file:src/main/resources/card-images/back.png";
 
     public boolean isFlipped() {
         return isFlipped;
     }
 
-    public void setFlipped(boolean flipped) {
+    private void setFlipped(boolean flipped) {
         isFlipped = flipped;
     }
 
+    public void flip() {
+        setFlipped(!isFlipped);
+
+        RotateTransition transition = new RotateTransition(Duration.millis(100), this);
+        transition.setAxis(Rotate.Y_AXIS);
+        transition.setFromAngle(180);
+        transition.setToAngle(90);
+
+        transition.setOnFinished(() -> {
+            if (isFlipped) {
+                setImage(getImage(backCardURI));
+            } else {
+                setImage(getImage(fileURI));
+            }
+            transition.setFromAngle(90);
+            transition.setToAngle(0);
+            transition.play();
+        });
+
+        transition.play();
+    }
+
     public Card(Enums size, Enums color, Enums container, Enums pattern) {
+        super(getImage("file:src/main/resources/card-images/"
+                + constructFileName(size, color, container, pattern)));
+
         this.size = size;
         this.color = color;
         this.container = container;
         this.pattern = pattern;
-        this.isBackCard = false;
         this.fileURI =
                 "file:src/main/resources/card-images/"
                         + constructFileName(size, color, container, pattern);
@@ -43,16 +78,6 @@ public class Card implements Packet {
 
     public String getCardName() {
         return cardName;
-    }
-
-    public Card() {
-        this.size = null;
-        this.color = null;
-        this.container = null;
-        this.pattern = null;
-        this.isBackCard = true;
-        this.fileURI = "file:src/main/resources/card-images/back.png";
-        this.cardName = "back.png";
     }
 
     public String getFileURI() {
@@ -76,10 +101,10 @@ public class Card implements Packet {
     }
 
     public boolean isBackCard() {
-        return isBackCard;
+        return isFlipped;
     }
 
-    private String constructFileName(Enums size, Enums color, Enums container, Enums pattern) {
+    private static String constructFileName(Enums size, Enums color, Enums container, Enums pattern) {
         return """
                 %s_%s_%s_%s.png
                 """
@@ -87,9 +112,12 @@ public class Card implements Packet {
     }
 
     public static Image getImage(String fileUri) {
+        if (fileUri.equals(backCardURI)) {
+            imageHashMap.get("back.png");
+        }
+
         String[] splitted = fileUri.split("/");
         final String fileName = splitted[splitted.length - 1];
-        System.out.println(fileName);
         if (imageHashMap.get(fileName) != null) {
             return imageHashMap.get(fileName);
         }
@@ -131,6 +159,49 @@ public class Card implements Packet {
         return matches;
     }
 
-    @Override
-    public void handle() {}
+    public String matches(Card other) {
+        if (this.size == other.size) {
+            return "size";
+        }
+        if (this.color == other.color) {
+            return "color";
+        }
+        if (this.container == other.container) {
+            return "container";
+        }
+        if (this.pattern == other.pattern) {
+            return "pattern";
+        }
+        return "none";
+    }
+
+    public static boolean isMatch(String against, Card... cards) {
+        Object ref;
+        switch (against) {
+            case "size" -> ref = cards[0].size;
+            case "color" -> ref = cards[0].color;
+            case "container" -> ref = cards[0].container;
+            case "pattern" -> ref = cards[0].pattern;
+            default -> throw new IllegalStateException("Unexpected value: " + against);
+        }
+
+        for (Card card : cards) {
+            switch (against) {
+                case "size" -> {
+                    if (card.size != ref) return false;
+                }
+                case "color" -> {
+                    if (card.color != ref) return false;
+                }
+                case "container" -> {
+                    if (card.container != ref) return false;
+                }
+                case "pattern" -> {
+                    if (card.pattern != ref) return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
