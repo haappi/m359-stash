6d78d203353d60c5f7cb112a415a4316ce74221d
haappi <74637670+haappi@users.noreply.github.com>
2023-04-04 09:12:26 -0500

    screw gradle

--- src/main/java/io/github/haappi/template/HelloController.java
+++ src/main/java/io/github/haappi/template/HelloController.java
@@ -1,13 +1,31 @@
 package io.github.haappi.template;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HelloController {
-  @FXML private Label welcomeText;
+    @FXML
+    private Button googleSignInButton;
 
-  @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
-  }
+    public void initialize() {
+        // Set the on-click event handler for the Google Sign-In button
+        googleSignInButton.setOnAction(event -> {
+            // Authenticate the user using Google Sign-In
+            FirebaseAuth.getInstance()
+                .signInWithPopup(new GoogleAuthProvider())
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // User is signed in
+                        FirebaseUser user = task.getResult().getUser();
+                        System.out.println("User signed in with Google: " + user.getDisplayName());
+                        // Redirect the user to the home screen
+                    } else {
+                        // Authentication failed
+                        System.out.println("Authentication failed: " + task.getException().getMessage());
+                    }
+                });
+        });
+    }
 }
