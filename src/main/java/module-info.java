82d364267b602f4d13e86ce6011dd8bdc08faa71
haappi <74637670+haappi@users.noreply.github.com>
2023-04-16 22:18:46 -0500

    gj.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,17 @@ module io.github.haappi.productivityApp {
   requires javafx.fxml;
   requires java.desktop;
     requires firebase.admin;
+    requires com.google.auth.oauth2;
+
+        // Requires directive for Firebase dependencies
+//    requires com.google.firebase.auth;
+//    requires com.google.firebase.database;
+//    requires com.google.firebase.firestore;
+//
+//    // Requires directive for Google Cloud dependencies
+//    requires com.google.cloud.storage;
+//    requires com.google.cloud.core;
+//    requires com.google.cloud.pubsub;
 
     opens io.github.haappi.productivityApp to
       javafx.fxml;
