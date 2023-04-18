ec8d99d4436c2eb0dca9a9b37fc4a8e7cafa35df
haappi <74637670+haappi@users.noreply.github.com>
2023-04-17 19:03:27 -0500

    build windows

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,9 @@ module io.github.haappi.ProductivityApp {
     requires javafx.fxml;
     requires com.gluonhq.attach.lifecycle;
     requires com.gluonhq.attach.util;
+    requires com.gluonhq.attach.display;
+    requires com.gluonhq.attach.storage;
 
-//    opens io.github.haappi to com.gluonhq.charm.glisten;
     opens io.github.haappi.views to javafx.fxml;
     exports io.github.haappi;
 }
