9cf2da0d0a00fcdb7681ee885383bc002cd8a096
haappi <74637670+haappi@users.noreply.github.com>
2023-05-06 13:58:27 -0500

    shi takes like 10m just to load [CI ios] [CI android] [CI windows]

--- src/main/java/io/github/haappi/HelloApplication.java
+++ src/main/java/io/github/haappi/HelloApplication.java
@@ -1,14 +1,20 @@
 package io.github.haappi;
 
+import com.gluonhq.attach.display.DisplayService;
+import com.gluonhq.attach.util.Services;
 import com.gluonhq.charm.glisten.application.AppManager;
 import com.gluonhq.charm.glisten.visual.Swatch;
 import io.github.haappi.views.PrimaryView;
 import io.github.haappi.views.SecondaryView;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Dimension2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 import static com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW;
 
@@ -18,6 +24,7 @@ public class HelloApplication extends Application {
     public static final String SECONDARY_VIEW = "Secondary View";
 
     private final AppManager appManager = AppManager.initialize(this::postInit);
+    private Stage stage;
 
     public static void main(String[] args) {
         launch(args);
@@ -33,8 +40,33 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Platform.runLater(() -> appManager.start(primaryStage));
-//        appManager.start(primaryStage);
+        this.stage = primaryStage;
+         Services.get(DisplayService.class).ifPresent(service -> {
+   Dimension2D resolution = service.getScreenResolution();
+      System.out.printf("Screen resolution: %.0fx%.0f", resolution.getWidth(), resolution.getHeight());
+  });
+//        loadFxmlFile("views/primary.fxml");
+//        Platform.runLater(() -> appManager.start(primaryStage));
+        appManager.start(primaryStage);
+    }
+
+    private void loadFxmlFile(String filename) {
+        filename = filename.replace(".fxml", "") + ".fxml";
+        String finalFilename = filename;
+        Platform.runLater(
+                () -> {
+                    FXMLLoader fxmlLoader =
+                            new FXMLLoader(HelloApplication.class.getResource(finalFilename));
+                    Scene scene = null;
+                    try {
+                        scene = new Scene(fxmlLoader.load(), 1600, 900);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    stage.setTitle("Hello!");
+                    stage.setScene(scene);
+                    stage.show();
+                });
     }
 
     private void postInit(Scene scene) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,12 @@ module io.github.haappi {
     requires com.gluonhq.charm.glisten;
     requires com.gluonhq.attach.lifecycle;
     requires com.gluonhq.attach.util;
-
+    requires com.gluonhq.attach.storage;
+    requires com.gluonhq.attach.statusbar;
+    requires com.gluonhq.attach.display;
 
     opens io.github.haappi to javafx.fxml;
+    opens io.github.haappi.views to javafx.fxml;
     exports io.github.haappi;
+    exports io.github.haappi.views;
 }
