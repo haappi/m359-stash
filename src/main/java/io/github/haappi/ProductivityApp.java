70d26678045f7b1493cd192f06ef38ab9db427cc
haappi <74637670+haappi@users.noreply.github.com>
2023-05-06 01:25:26 -0500

    hmmmmmmmmm

---
+++ src/main/java/io/github/haappi/ProductivityApp.java
@@ -0,0 +1,44 @@
+package io.github.haappi;
+
+import io.github.haappi.views.PrimaryView;
+import io.github.haappi.views.SecondaryView;
+import com.gluonhq.charm.glisten.application.AppManager;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import static com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW;
+
+public class ProductivityApp extends Application {
+
+    public static final String PRIMARY_VIEW = HOME_VIEW;
+    public static final String SECONDARY_VIEW = "Secondary View";
+
+    private final AppManager appManager = AppManager.initialize(this::postInit);
+
+    @Override
+    public void init() {
+        appManager.addViewFactory(PRIMARY_VIEW, () -> new PrimaryView().getView());
+        appManager.addViewFactory(SECONDARY_VIEW, () -> new SecondaryView().getView());
+
+        DrawerManager.buildDrawer(appManager);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        appManager.start(primaryStage);
+    }
+
+    private void postInit(Scene scene) {
+        Swatch.BLUE.assignTo(scene);
+
+        scene.getStylesheets().add(ProductivityApp.class.getResource("style.css").toExternalForm());
+        ((Stage) scene.getWindow()).getIcons().add(new Image(ProductivityApp.class.getResourceAsStream("/icon.png")));
+    }
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
-module io.github.haappi.fivechallenges {
+module io.github.haappi.ProductivityApp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.gluonhq.charm.glisten;
+    requires com.gluonhq.attach.lifecycle;
+    requires com.gluonhq.attach.util;
 
 
-    opens io.github.haappi.fivechallenges to javafx.fxml;
-    exports io.github.haappi.fivechallenges;
+    opens io.github.haappi to javafx.fxml;
+    exports io.github.haappi;
 }
